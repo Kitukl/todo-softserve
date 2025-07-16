b6ae978c2a8c0b3b7c5afde8644a3b156befc1e9
Ruslan Kit <ruslankit@Mac-mini-Ruslan.local>
2025-07-16 21:03:56 +0300

    created header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import { Button, Steps } from "antd"
+import { useEffect, useState } from "react"
+
+export const Header = () => {
+
+    const [date, setDate] = useState<string>('')
+
+    useEffect(() => {
+        const today = new Date().toLocaleDateString('uk-UA', {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+        });
+        setDate(today)
+    }, [])
+
+    return (
+        <header className="flex flex-row absolute z-50 bg-[#F8F8F8] border-b border-white w-full h-[6rem] shadow-lg items-center justify-around">
+            <h2 className="text-2xl text-blue-400 font-bold">To-Do by Oleh Kit</h2>
+            <span>{date}</span>
+            <div className="flex flex-row justify-evenly w-[15%]">
+                <Button type="default">Зареєструватись</Button>
+                <Button type="primary">Увійти</Button>
+            </div>
+        </header>
+    )
+}
